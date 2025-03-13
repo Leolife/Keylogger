68ed2a8e678a2c209bc41cf7b5253a2ec9317a89
Leo Roth <leoroth123@yahoo.com>
2025-03-13 12:27:37 -0700

    added a record keys on local machine option

---
+++ record-keys-on-local-machine/keylogger.py
@@ -0,0 +1,24 @@
+import keyboard
+
+
+class Keylogger:
+
+    def __init__(self):
+        self.is_recording = False
+
+    def startRecording(self):
+        self.is_recording = True
+        keyboard.start_recording()  # starts recording of keys
+
+    def stopRecording(self):
+        if self.is_recording:
+            self.is_recording = False
+            return keyboard.stop_recording()  # stops recording of keys
+        else:
+            raise Exception("There is no active recording to stop. Make sure to use 'startRecording()' first.")
+
+    def getTypedStrings(self, stoppedRecording):
+        return list(keyboard.get_typed_strings(stoppedRecording))  # returns the keys pressed
+
+    def writeKeysPressed(self, keysPressed):  # to be used in main for if 'enter' is pressed
+        return ''.join(keysPressed)  # turns list into string
